1134617c8f45bdef73d47b5707b2df65f837f7ae
CarlVoigt101 <89545053+CarlVoigt101@users.noreply.github.com>
2022-06-25 14:53:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1428,6 +1428,17 @@ function showPage() {
   document.getElementById("loaderContainer").style.display = "none";
 }
 
+AFRAME.registerComponent('thumbstick-logging',{
+  init: function () {
+    this.el.addEventListener('thumbstickmoved', this.logThumbstick);
+  },
+  logThumbstick: function (evt) {
+    if (evt.detail.y > 0.95) { console.log("DOWN"); }
+    if (evt.detail.y < -0.95) { console.log("UP"); }
+    if (evt.detail.x < -0.95) { console.log("LEFT"); }
+    if (evt.detail.x > 0.95) { console.log("RIGHT"); }
+  }
+});
 
 
 // var sectBool1 = true;
